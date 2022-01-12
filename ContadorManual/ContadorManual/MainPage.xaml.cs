9c62eb3697b3516309cd44e370b99713404fae50
fernandaSISTEM <fernanda15276@gmail.com>
2022-01-11 23:58:56 -0600

    subiendo modificado

---
+++ ContadorManual/ContadorManual/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ContadorManual
+{
+    public partial class MainPage:ContentPage
+    {
+        private int _Conteo;
+        public MainPage()
+        {
+            InitializeComponent();
+            _Conteo = 0;
+        }
+        private void ContarButton_Click(object sender, EventArgs e) {
+            _Conteo++; ConteoLabel.Text = _Conteo.ToString();
+        }
+        private void ReiniciarButton_Click(object sender, EventArgs e) {
+            _Conteo = 0; ConteoLabel.Text = _Conteo.ToString();
+        }
+
+        private void ContarButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+“#Aplicación que cuenta. Primera aplicación móvil con Xamarin.Forms.”
